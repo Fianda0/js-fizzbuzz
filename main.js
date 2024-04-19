faa756ad939e8177e5386ebb8126abaeadecbeb4
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-04-19 14:52:03 +0200

    add box in stampa

--- main.js
+++ main.js
@@ -9,18 +9,22 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         myDiv.append('FizzBuzz')
         mycontainer.append(myDiv)
+        myDiv.classList.add('bck-red')
         console.log('FizzBuzz')
     } else if (i % 3 == 0) {
         myDiv.append('Fizz')
         mycontainer.append(myDiv)
+        myDiv.classList.add('bck-green')
         console.log('Fizz')
     } else if (i % 5 == 0) {
         myDiv.append('Buzz')
         mycontainer.append(myDiv)
+        myDiv.classList.add('bck-yellow')
         console.log('Buzz')
     } else {
         myDiv.append(i)
         mycontainer.append(myDiv)
+        myDiv.classList.add('bck-blu')
         console.log(i)
     }
 }
